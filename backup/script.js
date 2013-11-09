038d83b40b0fef3203165b2ddcb37ff07ddc34dc
andrewfang <andrewbrianfang@gmail.com>
2013-11-09 01:12:48 -0800

    New version of website.  Moved old files to backup

---
+++ backup/script.js
@@ -0,0 +1,94 @@
+function show(event) {
+	if ($(this).parent().hasClass("disabled")) {
+		return false;
+	} else {
+		event.preventDefault();
+		var currenthash = $(this).attr("href");
+		$(".mainbody").fadeOut(100).delay(100);
+		window.location = currenthash;
+		$(".navbtn").removeClass("disabled");
+		if ($(this).parent().hasClass("navbtn")) {
+			$(this).parent().addClass("disabled");
+		}
+		$(currenthash).fadeIn();
+	}
+}
+
+function showCollab(event) {
+	if ($(this).parent().hasClass("disabled")) {
+		return false;
+	}
+	var currenthash = $(this).attr("href");
+	if ($(this).parent().siblings().hasClass("disabled")) {
+		$(".navbtn").removeClass("disabled");
+		$(this).parent().addClass("disabled");
+		window.location = currenthash;
+	}
+	else {
+		event.preventDefault();
+		$(".navbtn").removeClass("disabled");
+		$(".mainbody").fadeOut(100).delay(100);
+		$("#collab").show();
+		if ($(this).parent().hasClass("navbtn")) {
+			$(this).parent().addClass("disabled");
+		}
+    	window.location.hash = currenthash
+    	scrollToElement(currenthash);
+	}
+}
+
+function scrollToElement(selector, time, verticalOffset) {
+    time = typeof(time) != 'undefined' ? time : 1000;
+    verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
+    element = $(selector);
+    offset = element.offset();
+    offsetTop = offset.top + verticalOffset - 650;
+    $('html, body').animate({
+        scrollTop: offsetTop
+    }, time);
+}
+
+$.fn.scrollTo = function( target, options, callback ){
+  if(typeof options == 'function' && arguments.length == 2){ callback = options; options = target; }
+  var settings = $.extend({
+    scrollTarget  : target,
+    offsetTop     : 50,
+    duration      : 500,
+    easing        : 'swing'
+  }, options);
+  return this.each(function(){
+    var scrollPane = $(this);
+    var scrollTarget = (typeof settings.scrollTarget == "number") ? settings.scrollTarget : $(settings.scrollTarget);
+    var scrollY = (typeof scrollTarget == "number") ? scrollTarget : scrollTarget.offset().top + scrollPane.scrollTop() - parseInt(settings.offsetTop);
+    scrollPane.animate({scrollTop : scrollY }, parseInt(settings.duration), settings.easing, function(){
+      if (typeof callback == 'function') { callback.call(this); }
+    });
+  });
+}
+
+$(document).ready(function(){
+	$(".mainbody").hide();
+	$("#home").addClass("disabled");
+	$("#homebutton").on("click", show);
+	$("#aboutbutton").on("click", show);
+	$("#contactbutton").on("click", show);
+	$("#researchbutton").on("click", show);
+	if (window.location.hash == "") {
+		$("#home").show();
+	}
+	else if (window.location.hash.indexOf("collab") > 0) {
+		$("#collab").fadeIn();
+		$('body').scrollTo($(window.location.hash));
+	}
+	else{
+		$(window.location.hash).fadeIn();
+		$(window.location.hash + "button").addClass("disabled");	
+	}
+
+
+	$(".collabbutton").on("click", showCollab);
+
+	if (window.location.hash =="#resume") {
+		window.location = "resources/Resume.pdf";
+	}
+});
