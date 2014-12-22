e9941eab29132bf2bb648966aea1cdaa3b6c3157
Andrew Fang <andrewbrianfang@gmail.com>
2014-12-21 21:01:32 -0800

    Version 2 of the website now live

---
+++ php/head.php
@@ -0,0 +1,14 @@
+<?php
+echo '
+<link rel="icon" href="../resources/berkeleyLogo.gif" />
+<meta name="author" content="Andrew Fang">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="chrome=1">
+<link rel="stylesheet" media="screen" href="../css/stylesheet.css">
+<link rel="stylesheet" media="screen" href="../css/bootstrap.min.css">
+<link href="http://fonts.googleapis.com/css?family=Lato:100,300,400" rel="stylesheet" type="text/css">
+<script type="text/javascript" src="../js/jquery.min.js"></script>
+<script type="text/javascript" src="../js/bootstrap.min.js"></script>
+<script type="text/javascript" src="../js/script.js"></script>
+'
+?>
